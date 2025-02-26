dc87ed33e8ed7a0e9f652e3c1b0babf16895e222
Turkey12st <turkey12st@gmail.com>
2025-02-26 23:24:43 +0300

    Update Index.tsx

--- src/pages/accounting/Index.tsx
+++ src/pages/accounting/Index.tsx
@@ -1,4 +1,4 @@
-
+// src/pages/accounting/Index.tsx
 import { useState, useEffect } from "react";
 import { Card, CardContent, CardHeader, CardTitle } from "@/components/ui/card";
 import AppLayout from "@/components/AppLayout";
